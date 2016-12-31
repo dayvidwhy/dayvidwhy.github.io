842bfc06bbaf3de1144c8130e31e1d2a91aefd0a
David Young <dayvidwhy@gmail.com>
2016-12-31 18:51:01 +1000

    Add project tags, rename home to default layout.

--- js/index.js
+++
@@ -1,6 +0,0 @@
-var fontLoad = (function(font) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://fonts.googleapis.com/css?family=' + font;
-    document.head.appendChild(link);
-})('Open+Sans:400,500');
