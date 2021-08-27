e510380d3dd9d0e958215421d4f94d88e492070b
dayvidwhy <dayvidwhy@gmail.com>
2021-08-27 20:45:29 +1000

    Uses the github api to generate a list of projects.

---
+++ _plugins/jekyll_github.rb
@@ -0,0 +1,34 @@
+require 'octokit'
+require 'json'
+
+module Jekyll_GitHub
+    class Generator < Jekyll::Generator
+        safe true
+        priority :highest
+
+        def generate(site)
+            user = 'dayvidwhy'
+            client = Octokit::Client.new(:access_token => '')
+            repos = client.repos user
+            projects = Array.new
+            repos.each do |repo|
+                project = Hash.new
+                project['title'] = repo.name
+                project['description'] = repo.description
+                begin
+                    pages_info = client.pages repo.full_name
+                    project["link"] = pages_info.html_url
+                rescue
+                    project['link'] = repo.html_url
+                end
+                tags = client.topics repo.full_name
+                project['tech_tags'] = tags.names
+                project['developer'] = 'Web Developer'
+                project['meta'] = "Application Project"
+                project["theme_colour"] = "inherit"
+                projects.push(project)
+            end
+            site.data["projects"] = projects
+        end
+    end
+end
--- _plugins/time_to_read.rb
+++ _plugins/time_to_read.rb
@@ -1,15 +1,15 @@
-# Use {{ page.content | time_to_read }}
+# Usage {{ page.content | time_to_read }}
 module Jekyll
-  module TimeToReadFilter
-    def time_to_read(input)
-      words_per_minute = 180
+    module TimeToReadFilter
+        def time_to_read(input)
+        words_per_minute = 180
 
-      words = input.split.size;
-      minutes = (words / words_per_minute).floor;
-      minutes_label = minutes === 1 ? " minute" : " minutes";
-      minutes > 0 ? "about #{minutes} #{minutes_label}" : "less than 1 minute";
+        words = input.split.size;
+        minutes = (words / words_per_minute).floor;
+        minutes_label = minutes === 1 ? " minute" : " minutes";
+        minutes > 0 ? "about #{minutes} #{minutes_label}" : "less than 1 minute";
+        end
     end
-  end
 end
 
 Liquid::Template.register_filter(Jekyll::TimeToReadFilter);
